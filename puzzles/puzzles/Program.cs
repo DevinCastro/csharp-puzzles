613a52d932f4b3fcbcacea6b0ba939d0bce5c73c
DevinCastro <dcrocker21@gmail.com>
2020-11-25 05:40:15 -0800

    multiple coin tosses

--- puzzles/puzzles/Program.cs
+++ puzzles/puzzles/Program.cs
@@ -23,11 +23,61 @@ namespace puzzles
             return randNums;
         }
 
+        public static string TossCoin()
+        {
+            Console.WriteLine("Tossing a Coin!");
+            Random rand = new Random();
+            int num = rand.Next(0,2);
+            //Console.WriteLine(num);
+            
+            if (num == 0)
+            {
+                return "Heads";
+            } else
+            {
+                return "Tails";
+            }
+
+        }
+
+        public static double TossMultipleCoins(int num)
+        {
+            double heads = 0;
+            double tails = 0;
+            double ratio = 0.0;
+     
+
+            for (int i = 0; i < num; i++)
+            {
+                //TossCoin();
+                if (TossCoin() == "Heads")
+                {
+                    heads++;
+                } else
+                {
+                    tails++;
+                }
+            }
+            try
+            {
+                ratio = heads / tails;
+           
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("All Heads, No Tails.");
+            }
+
+            Console.WriteLine(heads + " / " + tails);
+            return ratio;
+
+        }
        
 
         static void Main(string[] args)
         {
-            RandomArray();
+            Console.WriteLine(TossMultipleCoins(5000));
+            // RandomArray();
             Console.ReadLine();
         }
     }
