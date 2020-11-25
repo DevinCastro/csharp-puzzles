91f3045643d4dab4d54e9526534338d5c87c6b2d
DevinCastro <dcrocker21@gmail.com>
2020-11-25 05:12:07 -0800

    create random array

--- puzzles/puzzles/Program.cs
+++ puzzles/puzzles/Program.cs
@@ -4,9 +4,24 @@ namespace puzzles
 {
     class Program
     {
+        public static int[] RandomArray()
+        {
+            int[] randNums = new int[10];
+            Random rand = new Random();
+
+            for (int i = 0; i < randNums.Length; i++)
+            {
+                randNums[i] = rand.Next(5, 26); // 26 will not be included, only to 25
+                Console.WriteLine(randNums[i]);
+            }
+
+            return randNums;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RandomArray();
+            Console.ReadLine();
         }
     }
 }
